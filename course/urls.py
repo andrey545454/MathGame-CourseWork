80b4e079bc3d34be61a00227412841b0c7b997ef
andrey545454 <superkurlin2013@yandex.ru>
2022-03-19 22:55:16 +0500

    Add index page

--- course/urls.py
+++ course/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('game.urls')),
+    path('game/', include('game.urls')),
     path('admin/', admin.site.urls),
 ]
