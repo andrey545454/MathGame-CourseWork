05709bd426b9cbc5004f86061a7556789abd7dd1
andrey545454 <superkurlin2013@yandex.ru>
2022-03-19 22:21:24 +0500

    Add config for DB

--- course/settings.py
+++ course/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,9 +77,7 @@ WSGI_APPLICATION = 'course.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-    }
+    'default': dj_database_url.config(conn_max_age=None, ssl_require=True)
 }
 
 
