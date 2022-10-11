22967fcaf365f4e186413b6ab77dc9174f4b8446
andrey545454 <superkurlin2013@yandex.ru>
2022-10-11 22:28:50 +0500

    fix env for debug

--- course/settings.py
+++ course/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = bool(os.getenv('APP_DEBUG'))
 
 ALLOWED_HOSTS = []
 
