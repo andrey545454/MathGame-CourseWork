1f5164547e44e72f918ed451af7e626772c0b924
andrey545454 <superkurlin2013@yandex.ru>
2022-10-11 23:08:06 +0500

    fix env

--- course/settings.py
+++ course/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('APP_DEBUG'))
+DEBUG = os.getenv('APP_DEBUG') == 'TRUE'
 
 ALLOWED_HOSTS = []
 
